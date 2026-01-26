d3f626ca79b022b16ec2d7acc0ec16c74d3570a7
Sergey Filkin <systema-plus@mail.ru>
2026-01-26 21:41:11 +0300

    Update SKILL.md with new query examples and fix formatting in run-1с-web.ps1

--- docs/run-1с-web.ps1
+++ docs/run-1с-web.ps1
@@ -1,1 +1,1 @@
-& "C:\Program Files\1cv8\8.3.27.1786\bin\ibsrv.exe" --db-path="D:\1C_Bases\unf_demo_base" --config="D:\1C_Projects\unf_demo\publication.yaml" --http-address="any"
+﻿& "C:\Program Files\1cv8\8.3.27.1786\bin\ibsrv.exe" --db-path="D:\1C_Bases\unf_demo_base" --config="D:\1C_Projects\unf_demo\publication.yaml" --http-address="any"
